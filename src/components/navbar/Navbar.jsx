1e771e66fad1f65e06bef8d5c99b3b4a4e13761e
Dilshoda <alimovadilshoda47@gmail.com>
2023-11-27 00:14:08 +0500

    navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+
+export default function Navbar () {
+    return (
+        <nav>
+
+        </nav>
+    )
+}
