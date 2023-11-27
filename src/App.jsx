52bf609f141c5db4360ed5db147021dfaa3aef5f
Dilshoda <alimovadilshoda47@gmail.com>
2023-11-27 09:28:08 +0500

    button and navbar is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
 
   return (
     <>
+    <Navbar/>
     </>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
+import Container from '../container/Container'
+import Button from '../button/Button';
 
 export default function Navbar () {
     return (
         <nav>
-
+            <Container>
+                <img src={logo} alt="" className='logo' />
+                <ul>
+                   <li><Link to='/'>Home</Link></li>
+                    <li><Link to='#'>Feature</Link></li>
+                    <li><Link to='#'>About</Link></li>
+                    <li><Link to='#'>Contact</Link></li>
+                </ul>
+                <Button text='shop now'/>
+            </Container>
         </nav>
     )
 }
